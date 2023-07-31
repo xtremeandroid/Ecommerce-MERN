a96da9ee11526308edb89f7f1363317910f035d4
Ayush Singh <mr.technical19@gmail.com>
2023-07-31 21:15:02 +0530

    fixedCartQty

--- frontend/src/slices/cartSlice.js
+++ frontend/src/slices/cartSlice.js
@@ -12,9 +12,7 @@ const cartSlice = createSlice({
     addToCart: (state, action) => {
       const item = action.payload;
 
-      const existItem = state.cartItems.find((x) => {
-        x._id === item._id;
-      });
+      const existItem = state.cartItems.find((x) => x._id === item._id);
 
       if (existItem) {
         state.cartItems = state.cartItems.map((x) =>
