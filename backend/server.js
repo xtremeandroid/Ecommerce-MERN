3516ea12d39148c8add90498c30e169721fc1258
Ayush Singh <mr.technical19@gmail.com>
2023-08-07 15:29:16 +0530

    update

--- backend/server.js
+++ backend/server.js
@@ -46,10 +46,10 @@ app.use("/api/upload", uploadRoutes);
 if (process.env.NODE_ENV === "production") {
   const __dirname = path.resolve();
   app.use("/uploads", express.static("/var/data/uploads"));
-  app.use(express.static(path.join(__dirname, "/frontend/build")));
+  app.use(express.static(path.join(__dirname, "/frontend/dist")));
 
   app.get("*", (req, res) =>
-    res.sendFile(path.resolve(__dirname, "frontend", "build", "index.html"))
+    res.sendFile(path.resolve(__dirname, "frontend", "dist", "index.html"))
   );
 } else {
   const __dirname = path.resolve();
