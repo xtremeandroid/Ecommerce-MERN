6f4f748611bf78d702094b0553b572a98d02338e
Ayush Singh <mr.technical19@gmail.com>
2023-08-11 21:01:04 +0530

    updates

--- frontend/src/components/Header.jsx
+++ frontend/src/components/Header.jsx
@@ -33,8 +33,8 @@ const Header = () => {
         <Container>
           <LinkContainer to="/">
             <Navbar.Brand>
-              <img src={logo} alt="ProShop" />
-              ProShop
+              <img src={logo} alt="PowerTech" />
+              PowerTech
             </Navbar.Brand>
           </LinkContainer>
           <Navbar.Toggle aria-controls="basic-navbar-nav" />
